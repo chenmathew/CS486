4103a7ad013b770befe8fcdb6b0a5665d6d3a335
Mathew Chen <mathewchenpnw@gmail.com>
2022-05-30 20:54:38 -0700

    push

---
+++ homework6.sql
@@ -0,0 +1,15 @@
+select
+  percentile_cont(0.25) within group (order by salary asc) as percentile_25,
+  percentile_cont(0.50) within group (order by salary asc) as percentile_50,
+  percentile_cont(0.75) within group (order by salary asc) as percentile_75
+from agent;
+
+select AVG(salary)
+from agent;
+select MODE(salary)
+from agent;
+
+select
+  MODE() within group (order by salary asc) as mode
+from agent;
+
